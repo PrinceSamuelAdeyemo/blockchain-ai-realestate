21f137abc279d1a4faec1b649e715c05745b41b9
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-04-21 07:54:45 +0100

    added views classes for the core app

--- backend/realestate_backend/core/views.py
+++ backend/realestate_backend/core/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import CustomUser, UserProfile, BlockchainWallet, KYCVerification, InvestorProfile
+from .serializers import (
+    CustomUserSerializer,
+    UserProfileSerializer,
+    BlockchainWalletSerializer,
+    KYCVerificationSerializer,
+    InvestorProfileSerializer
+)
 # Create your views here.
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+class BlockchainWalletViewSet(viewsets.ModelViewSet):
+    queryset = BlockchainWallet.objects.all()
+    serializer_class = BlockchainWalletSerializer
+    permission_classes = [IsAuthenticated]
+
+class KYCVerificationViewSet(viewsets.ModelViewSet):
+    queryset = KYCVerification.objects.all()
+    serializer_class = KYCVerificationSerializer
+    permission_classes = [IsAuthenticated]
+
+class InvestorProfileViewSet(viewsets.ModelViewSet):
+    queryset = InvestorProfile.objects.all()
+    serializer_class = InvestorProfileSerializer
+    permission_classes = [IsAuthenticated]
