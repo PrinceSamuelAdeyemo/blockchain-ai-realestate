d6a7afa2f9df44b8d9134a760aa06263126dbddc
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-04-27 22:39:03 +0100

    registered the models in the core app in the admin

--- backend/realestate_backend/core/admin.py
+++ backend/realestate_backend/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CustomUser, UserProfile, BlockchainWallet, KYCVerification, InvestorProfile
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
+admin.site.site_header = "Real Estate Core Admin"
+admin.site.site_title = "Real Estate Core Admin Portal"
+admin.site.index_title = "Welcome to the Real Estate Core Admin Portal"
+admin.site.register([
+    # Add your models here
+    CustomUser,
+    UserProfile,
+    BlockchainWallet,
+    KYCVerification,
+    InvestorProfile
+])
