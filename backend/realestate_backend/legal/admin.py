a9a98d204f14a39c327da8657cc8a62319844ddb
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-04-30 13:15:08 +0100

    added the models in the legal app to the admin site.

--- backend/realestate_backend/legal/admin.py
+++ backend/realestate_backend/legal/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ModelVersion, Prediction, TrainingData, FeatureSet
 
 # Register your models here.
+admin.site.register([
+    ModelVersion,
+    Prediction,
+    TrainingData,
+    FeatureSet
+])
