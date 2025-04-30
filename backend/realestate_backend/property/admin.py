b92947afb3f53653ba4c647e133fb2c6597cb5bc
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-04-30 13:22:34 +0100

    added the property models in the admin site

--- backend/realestate_backend/property/admin.py
+++ backend/realestate_backend/property/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from . models import (
+    Property,
+    PropertyType,
+    Amenity,
+    PropertyImage,
+    PropertyDocument
+)
 # Register your models here.
+admin.site.register([
+    Property,
+    PropertyType,
+    Amenity,
+    PropertyImage,
+    PropertyDocument
+])
