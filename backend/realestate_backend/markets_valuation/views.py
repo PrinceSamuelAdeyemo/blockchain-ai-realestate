b49ad54a0841e9f4b38b1d3925e219ef09d8bea4
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-04-30 13:17:42 +0100

    added and modified the views for the markets_valuation app

--- backend/realestate_backend/markets_valuation/views.py
+++ backend/realestate_backend/markets_valuation/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Valuation, MarketTrend, PriceHistory, NeighborhoodData
+from .serializers import (
+    ValuationSerializer,
+    MarketTrendSerializer,
+    PriceHistorySerializer,
+    NeighborhoodDataSerializer,
+)
 
 # Create your views here.
+
+
+class ValuationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing Valuation objects.
+    """
+    queryset = Valuation.objects.all()
+    serializer_class = ValuationSerializer
+
+
+class MarketTrendViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing MarketTrend objects.
+    """
+    queryset = MarketTrend.objects.all()
+    serializer_class = MarketTrendSerializer
+
+
+class PriceHistoryViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing PriceHistory objects.
+    """
+    queryset = PriceHistory.objects.all()
+    serializer_class = PriceHistorySerializer
+
+
+class NeighborhoodDataViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing NeighborhoodData objects.
+    """
+    queryset = NeighborhoodData.objects.all()
+    serializer_class = NeighborhoodDataSerializer
