14013511b81d08c4cea2e8eebf9e8af0fe47f851
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-04-30 13:13:46 +0100

    added the models for the governance in the admin site

--- backend/realestate_backend/governance/admin.py
+++ backend/realestate_backend/governance/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ModelVersion, Prediction, TrainingData, FeatureSet
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
+admin.site.register([
+    ModelVersion,
+    Prediction,
+    TrainingData,
+    FeatureSet
+])
