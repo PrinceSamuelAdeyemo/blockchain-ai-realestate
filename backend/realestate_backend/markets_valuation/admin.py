453349b00ffed227365b802acf22cca9be48ca33
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-04-30 13:18:20 +0100

    added the markets_valuation models in the admin site.

--- backend/realestate_backend/markets_valuation/admin.py
+++ backend/realestate_backend/markets_valuation/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from django.utils.translation import gettext_lazy as _
+from .models import Valuation, MarketTrend, PriceHistory, NeighborhoodData
 # Register your models here.
+admin.site.register([
+    Valuation,
+    MarketTrend,
+    PriceHistory,
+    NeighborhoodData
+])
