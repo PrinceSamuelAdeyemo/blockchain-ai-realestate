e8f3bfd93dce1c51cb215094c592b41276decbc8
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-06-09 10:43:21 +0100

    Update backend/realestate_backend/smartcontract/admin.py

--- backend/realestate_backend/smartcontract/admin.py
+++ backend/realestate_backend/smartcontract/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import (
+    ContractEvent,
+    SmartContract,
+    GasFeeRecord
+)
 # Register your models here.
+admin.site.register([
+    ContractEvent,
+    SmartContract,
+    GasFeeRecord
+])
