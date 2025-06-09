f004cc45492e43bf8320e7e36b140e555194708c
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-06-09 10:43:21 +0100

    Update backend/realestate_backend/smartcontract/views.py

--- backend/realestate_backend/smartcontract/views.py
+++ backend/realestate_backend/smartcontract/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import (
+    ContractEvent, 
+    SmartContract,
+    GasFeeRecord
+)
+from .serializers import (
+    ContractEventSerializer,
+    SmartContractSerializer, 
+    GasFeeRecordSerializer
+)
 
 # Create your views here.
+class ContractEventViewSet(viewsets.ModelViewSet):
+    queryset = ContractEvent
+    serializer_class = ContractEventSerializer
+    
+    
+class SmartContractViewSet(viewsets.ModelViewSet):
+    queryset = SmartContract
+    serializer_class = SmartContractSerializer
+    
+    
+class GasFeeRecordViewSet(viewsets.ModelViewSet):
+    queryset = GasFeeRecord
+    serializer_class = GasFeeRecordSerializer
