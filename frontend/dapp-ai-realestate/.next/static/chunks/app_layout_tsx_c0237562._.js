969474f033504a9b71f7de56e3d9eee48235f46f
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-06-09 10:44:14 +0100

    Update frontend/dapp-ai-realestate/.next/static/chunks/app_layout_tsx_c0237562._.js

--- frontend/dapp-ai-realestate/.next/static/chunks/app_layout_tsx_c0237562._.js
+++ frontend/dapp-ai-realestate/.next/static/chunks/app_layout_tsx_c0237562._.js
@@ -1,7 +1,18 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__9d126714._.css"
+  "static/chunks/node_modules_web3-errors_lib_esm_06a45c10._.js",
+  "static/chunks/node_modules_zod_lib_index_mjs_ee760afb._.js",
+  "static/chunks/node_modules_web3-utils_lib_esm_4760c1e0._.js",
+  "static/chunks/node_modules_web3-core_lib_esm_f5602f97._.js",
+  "static/chunks/node_modules_web3-eth-accounts_lib_esm_94edabb2._.js",
+  "static/chunks/node_modules_@noble_curves_esm_3ad603ac._.js",
+  "static/chunks/node_modules_web3-eth_lib_esm_b7a5a647._.js",
+  "static/chunks/node_modules_web3-eth-abi_lib_esm_c0b6496c._.js",
+  "static/chunks/node_modules_web3-eth-contract_lib_esm_848e0499._.js",
+  "static/chunks/node_modules_23498e2e._.js",
+  "static/chunks/[root-of-the-server]__b38c7271._.js",
+  "static/chunks/[root-of-the-server]__33bd1d78._.css"
 ],
     source: "dynamic"
 });
