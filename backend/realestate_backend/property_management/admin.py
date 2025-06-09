779851c2b5974e97a259e2c8b94712f73cc4809b
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-06-09 10:43:18 +0100

    Update backend/realestate_backend/property_management/admin.py

--- backend/realestate_backend/property_management/admin.py
+++ backend/realestate_backend/property_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Lease, Tenant, MaintenanceRequest, Inspection
 # Register your models here.
+admin.site.register([
+    Lease,
+    Tenant,
+    MaintenanceRequest,
+    Inspection
+    ])
