cb7251ee3ebef1589b19b4ae466534c64df0ffd2
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-06-09 10:43:19 +0100

    Update backend/realestate_backend/property_management/views.py

--- backend/realestate_backend/property_management/views.py
+++ backend/realestate_backend/property_management/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import (
+    LeaseSerializer, 
+    TenantSerializer,
+    MaintenanceRequestSerializer,
+    InspectionSerializer
+)
+from .models import (
+    Lease, 
+    Tenant,
+    MaintenanceRequest,
+    Inspection
+)
 # Create your views here.
+
+class LeaseViewSet(viewsets.ModelViewSet):
+    queryset = Lease.objects.all()
+    serializer_class = LeaseSerializer
+    
+    
+class TenantViewSet(viewsets.ModelViewSet):
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
+    
+    
+class MaintenanceRequestViewSet(viewsets.ModelViewSet):
+    queryset = MaintenanceRequest.objects.all()
+    serializer_class = MaintenanceRequestSerializer
+    
+    
+class InspectionViewSet(viewsets.ModelViewSet):
+    queryset = Inspection.objects.all()
+    serializer_class = InspectionSerializer
