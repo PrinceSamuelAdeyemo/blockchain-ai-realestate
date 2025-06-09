0011f245abbfd75e2ec1921a5e9b535d351b99e3
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-06-09 10:43:02 +0100

    Update backend/realestate_backend/core/admin.py

--- backend/realestate_backend/core/admin.py
+++ backend/realestate_backend/core/admin.py
@@ -3,9 +3,9 @@ from .models import CustomUser, UserProfile, BlockchainWallet, KYCVerification, 
 from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
-admin.site.site_header = "Real Estate Core Admin"
-admin.site.site_title = "Real Estate Core Admin Portal"
-admin.site.index_title = "Welcome to the Real Estate Core Admin Portal"
+#admin.site.site_header = "Real Estate Core Admin"
+#admin.site.site_title = "Real Estate Core Admin Portal"
+#admin.site.index_title = "Welcome to the Real Estate Core Admin Portal"
 admin.site.register([
     # Add your models here
     CustomUser,
