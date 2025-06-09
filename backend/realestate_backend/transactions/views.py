7ea05464dfc79234e2dd3450148ca9333535b6bb
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-06-09 10:43:23 +0100

    Update backend/realestate_backend/transactions/views.py

--- backend/realestate_backend/transactions/views.py
+++ backend/realestate_backend/transactions/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import (
+    Investment,
+    DividendPayout,
+    Transaction,
+    Escrow
+)
+from .serializers import (
+    InvestmentSerializer,
+    DividendPayoutSerializer,
+    TransactionSerializer,
+    EscrowSerializer
+)
 
 # Create your views here.
+
+class InvestmentViewSet(viewsets.ModelViewSet):
+    queryset = Investment.objects.all()
+    serializer_class = InvestmentSerializer
+    
+    
+class DividendPayoutViewSet(viewsets.ModelViewSet):
+    queryset = DividendPayout.objects.all()
+    serializer_class = DividendPayoutSerializer
+    
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    
+    
+class EscrowViewSet(viewsets.ModelViewSet):
+    queryset = Escrow.objects.all()
+    serializer_class = EscrowSerializer
