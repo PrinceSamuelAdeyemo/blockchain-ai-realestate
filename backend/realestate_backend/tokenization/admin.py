f51cd3f1644e7400e0eafc2f7c6dc462482c4c37
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-06-09 10:43:22 +0100

    Update backend/realestate_backend/tokenization/admin.py

--- backend/realestate_backend/tokenization/admin.py
+++ backend/realestate_backend/tokenization/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import (
+    TokenizedAsset,
+    TokenOwnership,
+    FractionalOwnership,
+    TokenTransaction
+)
 # Register your models here.
+admin.site.register([
+    TokenizedAsset, 
+    TokenOwnership, 
+    FractionalOwnership,
+    TokenTransaction
+])
